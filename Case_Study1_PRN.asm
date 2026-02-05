4b9afba5012f4c59d94874dfbd64b0c87cbdd801
tejasbawane <tbawane642@gmail.com>
2026-02-05 08:07:22 +0530

    Add files via upload

---
+++ Case_Study1_PRN.asm
@@ -0,0 +1,13 @@
+MOV A, #10H      
+MOV B, #04H      
+MUL AB           
+ADD A, #07H      
+                 
+MOV R0, #08H      
+INC R0            
+MOV B, #08H       
+
+                  
+MOV B, #10H       
+INC B             
+
